38778e21212b9f5274c002e5b0f6fe378b927e82
pichico <hitopmifukushima@yahoo.co.jp>
2016-10-08 22:36:01 +0900

    Initial Commit

---
+++ MapTodo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MapTodo
+//
+//  Created by Hitomi Fukushima on 2016/10/08.
+//  Copyright © 2016年 fukushima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
