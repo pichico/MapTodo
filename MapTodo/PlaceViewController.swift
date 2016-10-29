ac452b4159f6abc48d118416fa256ade00ccdff9
pichico <hitopmifukushima@yahoo.co.jp>
2016-10-29 09:37:10 +0900

    ジオフェンス機能なんとなく使ってみる

--- MapTodo/PlaceViewController.swift
+++ MapTodo/PlaceViewController.swift
@@ -6,9 +6,11 @@
 //  Copyright © 2016年 fukushima. All rights reserved.
 //
 
-import UIKit
 import CoreData
+import CoreLocation
 import MapKit
+import UIKit
+
 
 class PlaceViewController: UIViewController {
 
@@ -16,9 +18,9 @@ class PlaceViewController: UIViewController {
     @IBOutlet weak var mapView: MKMapView!
 
     var place: Place? = nil
-    var latitude: NSNumber? = nil
-    var longitude: NSNumber? = nil
+    var mapPoint: CLLocationCoordinate2D? = nil
     var todoEntities: [Todo] = []
+    var lm: CLLocationManager! = nil
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,10 +35,11 @@ class PlaceViewController: UIViewController {
             let predicate: NSPredicate = NSPredicate(format: "place = %@", argumentArray: [place!])
             todoEntities = Todo.mr_findAll(with: predicate) as! [Todo]
         } else {
-            // TODO それっぽい場所に移動
-            mapView.setRegion(MKCoordinateRegionMake(CLLocationCoordinate2DMake(35.6581, 139.701742),
-                                                 MKCoordinateSpanMake(0.005, 0.005)),
-                                                animated:true)
+            lm = CLLocationManager()
+            lm.delegate = self
+            lm.desiredAccuracy = kCLLocationAccuracyBest
+            lm.distanceFilter = 300
+            lm.startUpdatingLocation()
         }
     }
 
@@ -55,31 +58,16 @@ class PlaceViewController: UIViewController {
 
         //senderから長押しした地図上の座標を取得
         let tappedLocation = sender.location(in: mapView)
-        let tappedPoint = mapView.convert(tappedLocation, toCoordinateFrom: mapView)
-        latitude = tappedPoint.latitude as NSNumber?
-        longitude = tappedPoint.longitude as NSNumber?
+        mapPoint = mapView.convert(tappedLocation, toCoordinateFrom: mapView)
 
         //ピンの生成
         let pin = MKPointAnnotation()
         //ピンを置く場所を指定
-        pin.coordinate = tappedPoint
+        pin.coordinate = mapPoint!
+        print(mapPoint?.latitude)
+        print(mapPoint?.longitude)
         //ピンをMapViewの上に置く
         self.mapView.addAnnotation(pin)
-    }
-
-    func createPlace() {
-        let newPlace: Place = Place.mr_createEntity()!
-        newPlace.name = placeNameTextField.text
-        newPlace.latitude = latitude
-        newPlace.longitude = longitude
-        newPlace.managedObjectContext?.mr_saveToPersistentStoreAndWait()
-    }
-
-    func editTask() {
-        place?.name = placeNameTextField.text
-        place?.latitude = latitude
-        place?.longitude = longitude
-        place?.managedObjectContext?.mr_saveToPersistentStoreAndWait()
     }
 
     func replacePlace() {
@@ -87,13 +75,18 @@ class PlaceViewController: UIViewController {
             place = Place.mr_createEntity()!
         }
         place?.name = placeNameTextField.text
-        place?.latitude = latitude
-        place?.longitude = longitude
+        if mapPoint != nil {
+            place?.latitude = mapPoint!.latitude as NSNumber?
+            place?.longitude = mapPoint!.longitude as NSNumber?
+            // TODO radius指定できるようにする
+            lm.startMonitoring(for: CLCircularRegion.init(center: mapPoint!, radius: 200, identifier: place!.name!))
+        }
+        
         place?.managedObjectContext?.mr_saveToPersistentStoreAndWait()
     }
 
     @IBAction func save(_ sender: AnyObject) {
-        if latitude == nil {
+        if mapPoint == nil {
             let alert: UIAlertController = UIAlertController(title: "場所の指定がされていません", message: "近くに来たときに通知するには、地図を長押しして地点を指定して下さい", preferredStyle:  UIAlertControllerStyle.alert)
             let defaultAction: UIAlertAction = UIAlertAction(title: "場所を指定せずに保存", style: UIAlertActionStyle.default, handler:{
                 (action: UIAlertAction!) -> Void in
@@ -136,4 +129,34 @@ extension PlaceViewController: UITableViewDelegate, UITableViewDataSource {
         }
     }
 
+}
+
+extension PlaceViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        self.mapView.setRegion(MKCoordinateRegionMake(
+            CLLocationCoordinate2DMake(locations[0].coordinate.latitude, locations[0].coordinate.longitude),
+                                       MKCoordinateSpanMake(0.005, 0.005)), animated:true)
+    }
+    func locationManager(_ manager: CLLocationManager, didStartMonitoringFor region: CLRegion) {
+        print("start" + region.identifier)
+    }
+    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        print("enter region" + region.identifier)
+        let alert: UIAlertController = UIAlertController(title: "到着", message: region.identifier as String + "に到着", preferredStyle:  UIAlertControllerStyle.alert)
+        let defaultAction: UIAlertAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler:{
+            (action: UIAlertAction!) -> Void in
+        })
+        alert.addAction(defaultAction)
+        present(alert, animated: true, completion: nil)
+
+    }
+    func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
+        print("exit region" + region.identifier)
+        let alert: UIAlertController = UIAlertController(title: "出発", message: region.identifier as String + "を出発", preferredStyle:  UIAlertControllerStyle.alert)
+        let defaultAction: UIAlertAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler:{
+            (action: UIAlertAction!) -> Void in
+        })
+        alert.addAction(defaultAction)
+        present(alert, animated: true, completion: nil)
+    }
 }
