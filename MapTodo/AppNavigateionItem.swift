8e27545265de9c21a23f63b4335f8b32afa21767
pichico <hitopmifukushima@yahoo.co.jp>
2017-06-11 15:32:13 +0900

    TODOカラムのインデント揃える

---
+++ MapTodo/AppNavigateionItem.swift
@@ -0,0 +1,13 @@
+//
+//  AppNavigateionItem.swift
+//  MapTodo
+//
+//  Created by Hitomi Fukushima on 2017/06/11.
+//  Copyright © 2017年 fukushima. All rights reserved.
+//
+
+import UIKit
+
+class AppNavigateionItem: UINavigationItem {
+
+}
